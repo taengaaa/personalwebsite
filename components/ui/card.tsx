18024925a286454629d30f099c2d1693e62628fd
taengaa <simonaffentranger112@gmail.com>
2025-02-05 07:05:32 +0100

    Blog/Navigation

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+/* kommentar für git */
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
