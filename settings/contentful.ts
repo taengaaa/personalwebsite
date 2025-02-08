829a80b2f167f15ceea2af0ee496a628d3468006
taengaa <simonaffentranger112@gmail.com>
2025-02-08 12:14:39 +0100

    12

--- settings/contentful.ts
+++ settings/contentful.ts
@@ -1,3 +1,15 @@
-export const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
-export const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
-export const CONTENTFUL_PREVIEW_ACCESS_TOKEN = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+if (!process.env.CONTENTFUL_SPACE_ID) {
+  throw new Error('CONTENTFUL_SPACE_ID environment variable is not set');
+}
+
+if (!process.env.CONTENTFUL_ACCESS_TOKEN) {
+  throw new Error('CONTENTFUL_ACCESS_TOKEN environment variable is not set');
+}
+
+if (!process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN) {
+  throw new Error('CONTENTFUL_PREVIEW_ACCESS_TOKEN environment variable is not set');
+}
+
+export const CONTENTFUL_SPACE_ID: string = process.env.CONTENTFUL_SPACE_ID;
+export const CONTENTFUL_ACCESS_TOKEN: string = process.env.CONTENTFUL_ACCESS_TOKEN;
+export const CONTENTFUL_PREVIEW_ACCESS_TOKEN: string = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
